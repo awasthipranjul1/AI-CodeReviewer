730629309abed6d67c74fca6a56dbb68fa0521af
awasthipranjul1 <99887408+awasthipranjul1@users.noreply.github.com>
2025-02-15 13:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyD93jv-nUCK30PGZNJgShG0xI0kYocz_AU")
 
-sys_prompt = """You are a helpful AI Tutor for Data Science.
-Students will ask you doubts related to various topics in data science.
-You are expected to reply in as much detail as possible.
-Make sure to take examples while explaining a concept.
-In case if a student ask any question outside the data science scope,
-politely decline and tell them to ask the question from data science domain only."""
+sys_prompt = """You are a AI python code reviewer"""
 
 model = genai.GenerativeModel(model_name="models/gemini-1.5-flash",
                            system_instruction=sys_prompt)
